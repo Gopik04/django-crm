6854b6a79ffd168cb9e83f9d659154bcda499e03
Gopik04 <gopivkp04@gmail.com>
2024-09-12 00:22:16 +0530

    home page created and authendiction done for loggined user

--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    #check to log in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authendicate
+        user = authenticate(request, username=username ,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"you have been succesfully logedin")
+            return redirect('home')
+        else:
+            messages.success(request,"username or password is wrong please try again....")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def login_user(request):
+    pass
+def logout_user(request):
+    pass
